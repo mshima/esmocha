5a96615060bba6f880fa40026361ba2c7c2bf477
Marcelo Shima <marceloshima@gmail.com>
2023-05-04 20:42:37 -0300

    fix mocha exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * from 'mocha';
+export { after, afterEach, before, beforeEach, it, test, describe } from 'mocha';
 export * from 'mocha-expect-snapshot';
 export { jestExpect as expect } from 'mocha-expect-snapshot';
 export * from '@node-loaders/jest-mock';
+export * as esmocha from '@node-loaders/jest-mock';
