85877da3625292c78ece068b6fcef729adcf6cf3
Marcelo Shima <marceloshima@gmail.com>
2023-05-11 15:12:21 -0300

    allow --update-snapshot param

--- src/bin.cjs
+++ src/bin.cjs
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 const { join } = require('node:path');
+const process = require('node:process');
 const { pathToFileURL } = require('node:url');
+
+let updateSnapshotIndex = process.argv.indexOf('--update-snapshot');
+if (updateSnapshotIndex === -1) {
+  updateSnapshotIndex = process.argv.indexOf('--updateSnapshot');
+}
+
+if (updateSnapshotIndex > -1) {
+  let updateSnapshotValue = process.argv.length > updateSnapshotIndex + 1 ? process.argv[updateSnapshotIndex + 1] : 'all';
+  updateSnapshotValue = updateSnapshotValue === 'new' ? 'new' : 'all';
+  process.env.UPDATE_SNAPSHOT = updateSnapshotValue;
+}
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
 module.exports = (async () => {
