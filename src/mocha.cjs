95ef69f6b6d2a2d75ee95bcdb8b68745264bd1f3
Marcelo Boveto Shima <marceloshima@gmail.com>
2023-12-26 13:48:58 -0300

    fix sourcemaps with node 18.19 and 20

--- src/mocha.cjs
+++ src/mocha.cjs
@@ -1,8 +1,13 @@
 const Module = require('node:module');
+const process = require('node:process');
 
 if (Module.register) {
   const { pathToFileURL } = require('node:url');
   Module.register(pathToFileURL(require.resolve('./loader.js')));
+}
+
+if ('setSourceMapsEnabled' in process && typeof Error.prepareStackTrace !== 'function') {
+  process.setSourceMapsEnabled(true);
 }
 
 module.exports = require('mocha-expect-snapshot');
