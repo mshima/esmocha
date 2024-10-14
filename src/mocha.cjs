9f5312cfb927f4a0e83f1cdf911ba657d7d63473
Marcelo Shima <marceloshima@gmail.com>
2024-10-14 17:30:27 -0300

    feat: reset mocks on afterAll hooks

--- src/mocha.cjs
+++ src/mocha.cjs
@@ -2,12 +2,23 @@ const Module = require('node:module');
 const process = require('node:process');
 const { pathToFileURL } = require('node:url');
 /** @type {import('mocha').RootHookObject} */
-const { mochaHooks } = require('mocha-expect-snapshot');
+const snapshotHooks = require('mocha-expect-snapshot').mochaHooks;
 
 Module.register(pathToFileURL(require.resolve('@node-loaders/esbuild/strict')));
 
 if ('setSourceMapsEnabled' in process && typeof Error.prepareStackTrace !== 'function') {
   process.setSourceMapsEnabled(true);
 }
+
+/** @type {import('mocha').RootHookObject} */
+const mochaHooks = {
+  ...snapshotHooks,
+  async afterAll(...arguments_) {
+    const { reset, restoreAllMocks } = await import('./jest-mock.js');
+    reset(true);
+    restoreAllMocks();
+    await snapshotHooks.afterAll?.call(this, ...arguments_);
+  },
+};
 
 module.exports = { mochaHooks };
