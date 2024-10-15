8244a301c769a2064e403614e284363b287b1efb
Marcelo Shima <marceloshima@gmail.com>
2024-10-14 21:56:52 -0300

    feat: fix relative path mocking

--- src/jest-mock.ts
+++ src/jest-mock.ts
@@ -1,6 +1,8 @@
 import { ModuleMocker, type Mocked } from 'jest-mock';
+import { resolveCallerUrl } from './caller-resolve.js';
+import type Quibble from 'quibble';
 
-let quibble: typeof import('quibble').default;
+let quibble: typeof Quibble;
 const moduleMocker = new ModuleMocker(global);
 
 export const resetAllMocks = moduleMocker.resetAllMocks.bind(moduleMocker);
@@ -16,7 +18,7 @@ export const clearAllMocks = moduleMocker.clearAllMocks.bind(moduleMocker);
 export const isMockFunction = moduleMocker.isMockFunction.bind(moduleMocker);
 
 export const reset = (hard?: boolean) => {
-    quibble?.reset(hard);
+  quibble?.reset(hard);
 };
 
 /**
@@ -29,9 +31,15 @@ export async function mock<const MockedType = any>(
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   actual: Promise<MockedType> = import(specifier),
 ): Promise<Mocked<MockedType>> {
+  if (specifier.startsWith('.')) {
+    // Resolve relative paths since quibble will resolve having this file as the caller
+    specifier = new URL(specifier, resolveCallerUrl()).href;
+  }
+
   const metadata = moduleMocker.getMetadata<MockedType>(await actual)!;
   const mockReturn: Mocked<MockedType> | undefined = moduleMocker.generateFromMetadata(metadata);
-  quibble = (await import('quibble')).default;
+  const quibbleModule = await import('quibble');
+  quibble = quibbleModule.default;
   quibble(specifier, mockReturn);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   await quibble.esm(specifier, mockReturn as any);
