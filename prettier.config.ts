43a6f9bfddf3b06ee0402ab2f3147f97211d65da
Marcelo Shima <marceloshima@gmail.com>
2025-08-13 00:29:22 -0300

    convert prettier config to ts (#7)

---
+++ prettier.config.ts
@@ -0,0 +1,9 @@
+import { type Config } from 'prettier';
+
+export default {
+  printWidth: 140,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+} satisfies Config;
